ccdaabce3404df0c1520e166d7501abec7993988
PC-DOS <suicunexp@outlook.com>
2021-03-15 11:27:39 +0800

    Update interrupt-demo.c

--- interrupt-demo.c
+++ interrupt-demo.c
@@ -25,6 +25,10 @@
 /* Local header files */
 #include "interrupt-demo.h"
 
+struct class *interrupt_demo_class; //Device node 
+static int major = 0; //Allocate device ID automatically
+static struct cdev interrupt_demo_cdev; //cdev structure
+
 int interrupt_demo_open(struct inode *inode, struct file *filp){
     printk(KERN_INFO "InterruptDemo: Device file opending...\n");
     return 0;
@@ -59,6 +63,7 @@ static struct file_operations interrupt_demo_driver_file_operations = {
     .ioctl = interrupt_demo_ioctl, //IOControl, executed when calling ioctl()
 };
 
+/* Platform Device related functions */
 static int interrupt_demo_probe(struct platform_device *pdv){
 	printk(KERN_INFO "InterruptDemo: Initializing...\n");
 	return 0;
@@ -84,13 +89,60 @@ static int interrupt_demo_resume(struct platform_device *pdv){
 	return 0;
 }
 
+/* Init & Exit functions */
+static void interrupt_demo_setup_cdev(struct cdev *dev, int minor, struct file_operations *fops){ //Device setup functions, called by init()
+    int err, devno = MKDEV(major, minor);
+    cdev_init(dev, fops); //Initialize cdev
+    dev->owner = THIS_MODULE;
+    dev->ops = fops;
+    err = cdev_add(dev, devno, 1);
+    if(err){
+        printk(KERN_NOTICE "InterruptDemo: Error %d adding device %d.\n", err, minor);
+    }
+    printk("InterruptDemo: Device setup process finished.\n");
+}
+
 static int __init interrupt_demo_init(void){
 	printk(KERN_INFO "InterruptDemo: Initializing...\n");
+	int result;
+    dev_t dev = MKDEV(major, 0);
+    if(major){
+        //Static device ID
+        result = register_chrdev_region(dev, 1, DEVICE_NAME);
+        printk(KERN_INFO "InterruptDemo: register_chrdev_region().\n");
+    }
+    else{
+        //Allocate device ID
+        result = alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME);
+        printk(KERN_INFO "InterruptDemo: alloc_chrdev_region().\n");
+        major = MAJOR(dev);
+    }
+    if(result < 0){ //Errors occurred
+        printk(KERN_WARNING "InterruptDemo: alloc_chrdev_region() failed.\n");
+        return result;
+    }
+    interrupt_demo_setup_cdev(&interrupt_demo_cdev, 0, &interrupt_demo_driver_file_operations);
+    printk(KERN_INFO "InterruptDemo: The major device ID of this device is %d.\n", major);
+    //Use request_irq() to register interrupts here
+	
+    //Create device node
+    interrupt_demo_class = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(interrupt_demo_class)){
+        printk(KERN_INFO "InterruptDemo: failed in creating device class.\n");
+        return 0;
+    }
+    device_create(interrupt_demo_class, NULL, dev, NULL, NODE_NAME);
 	return 0;
 }
 
 static void __exit interrupt_demo_exit(void){
 	printk(KERN_INFO "InterruptDemo: Exiting...\n");
+	device_destroy(interrupt_demo_class,MKDEV(major, 0));
+    class_destroy(interrupt_demo_class);
+    cdev_del(&interrupt_demo_cdev);
+    unregister_chrdev_region(MKDEV(major, 0), 1)
+    //Use free_irq() to unregister interrupts here
+	
 	return;
 }
 
@@ -110,7 +162,6 @@ struct platform_driver interrupt_demo_driver{
 		.owner = THIS_MODULE
 	}
 };
-
 
 /* Licenses, Authors & Descriptions */
 MODULE_LICENSE("Dual BSD/GPL");
