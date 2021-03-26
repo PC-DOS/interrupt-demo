54e7adce967f5d71785a0b1fee1961fcb711ee24
PC-DOS <suicunexp@outlook.com>
2021-03-26 10:16:23 +0800

    Update MathFunctions.c

--- MathFunctions.c
+++ MathFunctions.c
@@ -6,11 +6,14 @@
 inline long GetMin(long iNum1, long iNum2){
 	return iNum1<iNum2 ? iNum1 : iNum2;
 }
+EXPORT_SYMBOL(GetMin);
 
 inline long GetMax(long iNum1, long iNum2){
 	return iNum1>iNum2 ? iNum1 : iNum2;
 }
+EXPORT_SYMBOL(GetMax);
 
 inline long GetAbs(long iNum){
 	return iNum>=0 ? iNum : -iNum;
 }
+EXPORT_SYMBOL(GetAbs);
