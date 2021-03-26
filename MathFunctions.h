317d7df35bc3fd7b4bd29d3a6b202ac36cbf9395
PC-DOS <suicunexp@outlook.com>
2021-03-26 10:17:45 +0800

    Math

--- MathFunctions.h
+++ MathFunctions.h
@@ -2,7 +2,10 @@
 #define _MATH_FUNCTIONS_H
 
 inline long GetMin(long iNum1, long iNum2);
+EXPORT_SYMBOL(GetMin);
 inline long GetMax(long iNum1, long iNum2);
+EXPORT_SYMBOL(GetMax);
 inline long GetAbs(long iNum);
+EXPORT_SYMBOL(GetAbs);
 
 #endif
